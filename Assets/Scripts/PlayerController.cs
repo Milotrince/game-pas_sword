7e39496f28bfde3e1c91f1cb530843ab7c595b62
Milotrince <trinityjchung@gmail.com>
2020-12-05 16:49:48 -0800

    Add sword collision with enemy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,28 +13,21 @@ public class PlayerController: MonoBehaviour
 
     private Vector2 _moveVector;
     private PlayerInputAction _inputActions;
-    private SpriteRenderer[] _spriteRenderers;
-    private Transform[] _childTransforms;
-    private SwordRenderer _swordRenderer;
+    private SwordController _swordController;
     private Sword _sword;
     private bool _flipped = false;
-    private bool _swinging = false;
 
     void Awake()
     {
         _sword = new Sword(new Password("0123456789"));
-        _swordRenderer = GetComponentInChildren<SwordRenderer>();
-        _swordRenderer.SetSword(_sword);
+        _swordController = GetComponentInChildren<SwordController>();
+        _swordController.SetSword(_sword);
 
         _inputActions = new PlayerInputAction();
         _inputActions.Enable();
-        _spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
-        var allTransforms = new List<Transform>(GetComponentsInChildren<Transform>());
-        allTransforms.Remove(transform); // Don't want to flip parent
-        _childTransforms = allTransforms.ToArray();
 
         // attack
-        _inputActions.Player.Attack.performed += _ => SwingSword();
+        _inputActions.Player.Attack.performed += _ => _swordController.SwingSword();
     }
 
     void onEnable()
@@ -66,43 +59,11 @@ public class PlayerController: MonoBehaviour
         bool shouldFlipRight = _flipped && _moveVector.x > epsilon;
         if (shouldFlipLeft || shouldFlipRight)
         {
-            foreach (SpriteRenderer sr in _spriteRenderers)
-            {
-                sr.flipX = !sr.flipX;
-            }
-            foreach (Transform tf in _childTransforms)
-            {
-                tf.localPosition = new Vector3(-tf.localPosition.x, tf.localPosition.y, 0f);
-            }
+            transform.localScale = new Vector3(-transform.localScale.x, 1, 1);
             _flipped = !_flipped;
         }
     }
 
-    private void SwingSword() {
-        if (!_swinging)
-            StartCoroutine(SwingAnimation());
-    }
 
-    IEnumerator SwingAnimation()
-    {
-        _swinging = true;
-
-        Transform tf = _swordRenderer.gameObject.transform;
-        float angle = _sword.SwingAngle/2f;
-        float targetAngle = -angle;
-        int sign = _flipped ? -1 : 1;
-        tf.localRotation = Quaternion.Euler(0, 0, angle * sign);
-
-        while (angle > targetAngle) {
-            float percent = (angle-targetAngle)/_sword.SwingAngle;
-            angle -= _sword.SwingSpeed * (2-percent);
-            tf.localRotation = Quaternion.Euler(0, 0, angle * sign);
-            yield return new WaitForFixedUpdate();
-        }
-        yield return new WaitForSeconds(1f/_sword.SwingSpeed);
-
-        tf.localRotation = Quaternion.identity;
-        _swinging = false;
-    }
 
 }
---
+++ Assets/Scripts/SwordController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour
+{
+
+
+    private Sword _sword;
+    private bool _swinging = false;
+    private BoxCollider2D _collider;
+    private SpriteRenderer _spriteRenderer;
+
+
+    public void SetSword(Sword sword)
+    {
+        this._sword = sword;
+        CreateComponents();
+    }
+
+    public void SwingSword() {
+        if (!_swinging)
+            StartCoroutine(SwingAnimation());
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Enemy")
+        {
+            Debug.Log("sword hit enemy");
+        }
+
+    }
+
+    private Vector2Int CalculateSwordSize()
+    {
+        int width = Mathf.Clamp( Mathf.RoundToInt(_sword.BaseDamage/ 100f), 1, 5 );
+        int length = _sword.Password.String.Length;
+        return new Vector2Int(length, width+2);
+    }
+
+    private void CreateComponents()
+    {
+        Vector2Int size = CalculateSwordSize();
+
+        _collider = gameObject.AddComponent<BoxCollider2D>() as BoxCollider2D;
+        _collider.size = new Vector2(size.x, size.y) / 8f;
+        _collider.offset = _collider.size / 2f * Vector2.down;
+        _collider.enabled = false;
+
+        _spriteRenderer = gameObject.AddComponent<SpriteRenderer>() as SpriteRenderer;
+        DrawSwordSprite(size);
+    }
+
+    private void DrawSwordSprite(Vector2Int size)
+    {
+        string password = _sword.Password.String;
+
+        // TODO: make not arbitrary calculation
+        Color color = Color.HSVToRGB(Random.value, 1, 1);
+
+        // texture and sword size oriented →
+        Texture2D texture = new Texture2D(size.x, size.y, TextureFormat.RGBA32, false);
+
+        Color[] allClearPixels = new Color[size.x * size.y];
+        for (int i = 0; i < allClearPixels.Length; i++)
+        {
+            allClearPixels[i] = Color.clear;
+        }
+        texture.SetPixels(allClearPixels);
+
+        // Hilt
+        texture.SetPixel(1, 0, color);
+        texture.SetPixel(1, size.y-1, color);
+
+        // Blade
+        for (int i = 0; i < size.x; i++)
+        {
+            for (int j = 1; j < size.y-1; j++)
+            {
+                texture.SetPixel(i, j, color);
+            }
+        }
+
+        texture.filterMode = FilterMode.Point;
+        texture.Apply();
+
+        Rect rect = new Rect(Vector2.zero, size);
+        Vector2Int pivot = new Vector2Int(0, Mathf.CeilToInt(size.y/2f)-1);
+        Sprite swordSprite = Sprite.Create(texture, rect, pivot, 8f);
+
+        _spriteRenderer.sprite = swordSprite;
+    }
+
+    private IEnumerator SwingAnimation()
+    {
+        _swinging = true;
+        _collider.enabled = true;
+
+        float angle = _sword.SwingAngle/2f;
+        float targetAngle = -angle;
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
+
+        while (angle > targetAngle) {
+            float percent = (angle-targetAngle)/_sword.SwingAngle;
+            angle -= _sword.SwingSpeed * (2-percent);
+            transform.localRotation = Quaternion.Euler(0, 0, angle);
+            yield return new WaitForFixedUpdate();
+        }
+        _collider.enabled = false;
+        yield return new WaitForSeconds(1f/_sword.SwingSpeed);
+
+        transform.localRotation = Quaternion.identity;
+        _swinging = false;
+    }
+
+}
